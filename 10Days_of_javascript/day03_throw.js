471153e97ea6f959e9eeed65cc52a53d3e707540
Chang <cnxiong03@gmail.com>
2020-07-22 23:30:14 -0400

    solution with throw conditions

--- 10Days_of_javascript/day03_throw.js
+++ 10Days_of_javascript/day03_throw.js
@@ -1,14 +1,28 @@
-function isPositive(a) {
-  let YES = "YES";
-  let ZeroError = "Zero Error";
-  let negaitve = "Negative Error";
+// First approach
 
-  if (a >= 1) {
-    return YES;
+// function isPositive(a) {
+//   let YES = "YES";
+//   let ZeroError = "Zero Error";
+//   let negaitve = "Negative Error";
+
+//   if (a >= 1) {
+//     return YES;
+//   } else if (a == 0) {
+//     return ZeroError;
+//   } else if (a < 0) {
+//     return negaitve;
+//   }
+// }
+// isPositive(-9);
+
+function isPositive(a) {
+  if (a > 0) {
+    return "YES";
   } else if (a == 0) {
-    return ZeroError;
-  } else if (a < 0) {
-    return negaitve;
+    throw new Error("Zero Error");
+  } else {
+    throw new Error("Negative Error");
   }
 }
+
 isPositive(-9);
