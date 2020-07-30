c94c255598b62392b04ff71523c390fc525b6612
Chang <cnxiong03@gmail.com>
2020-07-30 00:29:39 -0400

    updated code

--- 10Days_of_javascript/Day04_classes.js
+++ 10Days_of_javascript/Day04_classes.js
@@ -1,16 +1,23 @@
 // Implement a Polygon class
 class Polygon {
   // constructor that takes an array of integer side lengths
-  constructor(heights) {
-    this.heights = heights;
+  constructor(array) {
+    this.array = array;
   }
   // perimeter method that returns the sum of the Polygon's side lengths
   perimeter() {
     let res = 0;
-    for (let i = 0; i < this.heights; i++) {
-      res += this.heights[i];
+    for (let i = 0; i < this.array.length; i++) {
+      res += this.array[i];
     }
     return res;
   }
 }
-let trianlge = new Polygon([3, 4, 5]);
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
