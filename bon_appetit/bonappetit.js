6878340b75274147a744742597b6298e90d5304a
Chang <cnxiong03@gmail.com>
2020-08-16 16:33:27 -0400

    added alternative solution

--- bon_appetit/bonappetit.js
+++ bon_appetit/bonappetit.js
@@ -14,3 +14,11 @@ function bonAppetit(bill, k, b) {
 }
 console.log(bonAppetit([3, 10, 2, 9], 1, 12));
 console.log(bonAppetit([3, 10, 2, 9], 1, 7));
+
+// Alternative solution
+// function bonAppetit(bill, k, b) {
+//   const sum = bill.reduce((r, v) => r + v, 0);
+//   const result = (sum - bill[k]) / 2;
+//   if (b - result === 0) console.log("Bon Appetit");
+//   else console.log(b - result);
+// }
