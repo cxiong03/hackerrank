1937b05a4779e8e3a1b9b908f14571456a019d2d
Chang <cnxiong03@gmail.com>
2020-08-17 20:26:56 -0400

    added another solution

--- jumping_in_the_clouds/jumpingintheclouds.js
+++ jumping_in_the_clouds/jumpingintheclouds.js
@@ -24,3 +24,15 @@ function jumpingOnClouds(c) {
   return count;
 }
 jumpingOnClouds([0, 1, 0, 0, 0, 1, 0]);
+
+// Alternative solution
+// function jumpingOnClouds(c) {
+//     let jump = 0;
+//     for (let i = 0; i < c.length -1; i+=2) {
+//         jump++;
+//         if(c[i] == 1) {
+//             i -=1;
+//         }
+//     }
+//     return jump;
+// }
