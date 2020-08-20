b3a5ee8cc8b7a468345b41ab4fcde64665391d23
Chang <cnxiong03@gmail.com>
2020-08-20 19:05:10 -0400

    finding the area of highlighted word base on the height of the tallest letter times length of word

---
+++ designer_pdf_viewer/designerpdf.js
@@ -0,0 +1,46 @@
+function designerPdfViewer(height, letters) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var maxHeight = 0;
+  for (
+    var locationOfLetter = 0;
+    locationOfLetter < letters.length;
+    locationOfLetter++
+  ) {
+    var result = alphabet.indexOf(letters[locationOfLetter]);
+    if (height[result] > maxHeight) {
+      maxHeight = height[result];
+    }
+  }
+  let volume = maxHeight * letters.length;
+  return volume;
+}
+let height = [
+  1,
+  3,
+  1,
+  3,
+  1,
+  4,
+  1,
+  3,
+  2,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+  5,
+];
+let word = "abc";
+console.log(designerPdfViewer(height, word));
